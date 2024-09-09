9dd70129e108471edbd3e45f5c2900a0390a5392
UJJWAL <ujjwal8851114020@gmail.com>
2024-09-09 23:29:48 +0530

    long time

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from "react-chat-engine";
+import './App.css';
+
+const App=()=>{
+    return(
+        <ChatEngine
+           height='100vh'
+           projectID='e53fe8db-0eb7-4dba-88b5-149a42507b50'
+           userName='GiggaNigga'
+           userSecret='yfV4qnvv6**zDqa'
+        />
+    )
+}
+
+export default App;
